b27c4f2710e8d8a7a2527e2ca7982c728cc2126f
yandrade1305 <yan13052001@gmail.com>
2022-07-08 12:28:39 -0300

    Alterando classe abstrata

--- miltankbank/src/main/java/br/com/miltankbank/service/executor/abstracts/AbstractExecutorCadastroDespesa.java
+++ miltankbank/src/main/java/br/com/miltankbank/service/executor/abstracts/AbstractExecutorCadastroDespesa.java
@@ -6,7 +6,7 @@ import br.com.miltankbank.model.entity.Despesa;
 import br.com.miltankbank.model.repository.DespesaRepository;
 import br.com.miltankbank.service.acao.AcaoCadastroDespesa;
 
-public class abstract AbstractExecutorCadastroDespesa implements AcaoCadastroDespesa{
+public abstract class AbstractExecutorCadastroDespesa implements AcaoCadastroDespesa{
 
     private final DespesaRepository despesaRepository;
 
